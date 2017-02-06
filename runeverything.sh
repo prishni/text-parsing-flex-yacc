cb332b406793a9393f3128c65c3234d81be0dbf8
Prishni Rateria <prishni93rateria@gmail.com>
2017-02-07 01:46:52 +0530

    Add files via upload

--- runeverything.sh
+++ runeverything.sh
@@ -5,5 +5,6 @@ python3 main.py
 rm ./databaseinp/.DS_Store
 rm ./databaseinp/.csv
 python queryprocessng.py
+/Applications/XAMPP/xamppfiles/bin/mysql -u root csdb < drop_tables.sql
 /Applications/XAMPP/xamppfiles/bin/mysql -u root csdb < createtable.sql
 /Applications/XAMPP/xamppfiles/bin/mysql -u root csdb < updatetable.sql
